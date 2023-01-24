df72298b473f7bc0e59985415eb8aa71d62bb89a
Alex <jam.madrigal@gmail.com>
2023-01-23 19:53:10 -0800

    Time: 66 ms (79.80%), Space: 42.3 MB (73.97%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -4,26 +4,48 @@
  * @return {boolean}
  */
 var backspaceCompare = function(a, b) {
-    // brute force, loop through each array, pushing to a new string then popping off if the current character is a #
-  // Initialize empty strings
-  let one = [];
-  let two = [];
-  // return if they are equal or not
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === "#") {
-      one.pop();
-    } else {
-      one.push(a[i]);
+  // Use two pointers, more efficient solution which improves space complexity
+  // Start from the end of each string
+  // If the current character is a #, decrement the corresponding pointer by 2
+  // If at any point the characters do not match after this process, return false, else return true
+  let aPointer = a.length - 1;
+  let bPointer = b.length - 1;
+    while (aPointer >= 0 || bPointer >= 0) {
+    // This part was tricky. You have to store the accumulation of # in variables to get an accurate decrement, while also checking if what you decremented to is another hash and should increase the backcount further. 
+     if (a[aPointer] === "#" || b[bPointer] === "#") {
+            if(a[aPointer] === "#") {
+                let decrement = 2;
+                
+                while(decrement > 0) {
+                    aPointer--;
+                    decrement--;
+                    
+                    if(a[aPointer] === "#") {
+                        decrement += 2;
+                    }
+                }
+            }
+            
+            if(b[bPointer] === "#") {
+                let decrement = 2;
+                
+                while(decrement > 0) {
+                    bPointer--;
+                    decrement--;
+                    
+                    if(b[bPointer] === "#") {
+                        decrement += 2;
+                    }
+                }
+            }
+     } 
+         
+      if (a[aPointer] !== b[bPointer]) {
+          return false
+      } else {
+        aPointer--;
+        bPointer--;
+      }
     }
-    
-  }
-
-  for (let i = 0; i < b.length; i++) {
-    if (b[i] === "#") {
-      two.pop();
-    } else {
-      two.push(b[i]);
-    }
-  }
-    return(JSON.stringify(one) === JSON.stringify(two));  
+  return true; 
 };
