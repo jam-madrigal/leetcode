eead8ba5791bba3b3ecf206e842e9d804de098ce
Alex <jam.madrigal@gmail.com>
2023-01-26 22:28:39 -0800

    Time: 959 ms (5.02%), Space: 48.7 MB (32.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -18,7 +18,7 @@ var lengthOfLongestSubstring = function(s) {
     //use two pointers in a while loop while pointer1 is less than length-1, one moves ahead while the other saves the starting point to make strings
     let pointer1 = 0;
     let pointer2 = pointer1;
-    while (pointer1 !== s.length) {
+    while (pointer1 !== s.length || pointer2 !== s.length) {
         //if the character is already included in the array, or if the end of the string (length - 1) is reached by pointer2, compare the length to the current longest length and update it if it is larger and increment pointer1 and clear the string, reset pointer2, otherwise, push s[pointer2], increment the second pointer and repeat this process 
         if (string.includes(s[pointer2]) || pointer2 === s.length) {
             if (string.length > longestLength) {
