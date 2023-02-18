8f5657a0c7082beb74cc157d2646c268bb018c00
Alex <jam.madrigal@gmail.com>
2023-02-17 16:19:29 -0800

    Time: 129 ms (81.87%), Space: 49.1 MB (82.83%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -33,25 +33,38 @@ const getPartition = function (nums, left, right) {
 
 };
 
-const quickSort = function (nums, left, right) {
+const quickSelect = function (nums, left, right, indexToFind) {
   // This function should only run while the left is less than the right, otherwise it's already sorted/only has one index
-  if (left < right) {
-    // First, the actual sorting happens in an external function, that's where we determine our pivot and sort, this function just calls that function and then uses the results to call itself again until it successfully divides and conquers the whole array
+  // // if (left < right) {
+    // // For Quicksort
+    // // First, the actual sorting happens in an external function, that's where we determine our pivot and sort, this function just calls that function and then uses the results to call itself again until it successfully divides and conquers the whole array
+    // const partitionIndex = getPartition(nums, left, right);
+    // // After getting the partitionIndex, which is essentially to where we end up moving our pivot, call the function again using that value
+    // // Calls the function again, but breaking apart the array into everything less than the pivot
+    // quickSort(nums, left, partitionIndex - 1);
+    // // Do the same for everything greater than the pivot, completing our division and further dividing and sorting, by the time this finishes, our array will have been sorted and everything returns back to the final step of the findKthLarget function
+    // quickSort(nums, partitionIndex + 1, right);
+    // Optimal quickselect solution just for this problem. Adds conditionals and takes in the new fourth variable, indexToFind. The underlying logic is the same, but this algorithm doesn't care about sorting the whole array. Instead, we sort the first pass like usual, but when we recursively call the function, we won't bother sorting the left side. This is because if we make it that far then we know the solution has to be on the right side already, and instead of sorting everything we can just return the correct index once we've sorted to the point at which the partitionIndex is equal to the index we're trying to find. It prevents  doing extraneous calculations beyond finding the index we need and only that. It's the same as the other quickSort solution basically, but it adds checks for this, and that alone makes it much more efficient. I definitely overthought this at first when thinking about how quickSelect was explained to work, but seeing the code like this, it makes a lot of sense.
     const partitionIndex = getPartition(nums, left, right);
-    // After getting the partitionIndex, which is essentially to where we end up moving our pivot, call the function again using that value
-    // Calls the function again, but breaking apart the array into everything less than the pivot
-    quickSort(nums, left, partitionIndex - 1);
-    // Do the same for everything greater than the pivot, completing our division and further dividing and sorting, by the time this finishes, our array will have been sorted and everything returns back to the final step of the findKthLarget function
-    quickSort(nums, partitionIndex + 1, right);
-  }
+    if (partitionIndex === indexToFind) {
+    return nums[partitionIndex];
+    } else if (indexToFind < partitionIndex) {
+      return quickSelect(nums, left, partitionIndex - 1, indexToFind);
+    } else {
+      return quickSelect(nums, partitionIndex + 1, right, indexToFind);
+    }
 };
 
 // The function to solve our overall problem will have to be the start of a divide and conquer/quicksort approach
 var findKthLargest = function (nums, k) {
   // First, it saves the index that we need to find in the end, which is the index k values from the end of our array
   const indexToFind = nums.length - k;
-  // Then, we start the quicksorting, passing in the start and end points for our first division of the problem into sub problems, which always uses the beginning and end of the full array
-  quickSort(nums, 0, nums.length - 1);
-  // Once that recursion all finishes, we know the array has been sorted, so return it
-  return nums[indexToFind];
+  // // Then, we start the quicksorting, passing in the start and end points for our first division of the problem into sub problems, which always uses the beginning and end of the full array
+    //// for quickSort
+  // quickSort(nums, 0, nums.length - 1);
+  // // Once that recursion all finishes, we know the array has been sorted, so return it
+  // return nums[indexToFind];
+    
+    // Optimized quickSelect
+    return quickSelect(nums, 0, nums.length - 1, indexToFind);
 };
