8b1a323a74e8c8d5bffaac86d828b9da13b45323
Alex <jam.madrigal@gmail.com>
2023-06-25 20:19:27 -0700

    Time: 269 ms (65.38%), Space: 119 MB (20.60%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+var numOfMinutes = function(n, headID, managers, informTime) {
+  const adjList = [];
+  
+  for (let i = 0; i < n; i++) { 
+    adjList.push([]);
+  }
+
+  for(let employee = 0; employee < n; employee++) {
+  const manager = managers[employee];
+  if(manager === -1) continue;
+    
+    adjList[manager].push(employee);
+  }
+  
+  return dfs(headID, adjList, informTime);
+}
+
+const dfs = function(currentId, adjList, informTime) {
+  if(adjList[currentId].length === 0) {
+    return 0;
+  }
+  
+  let max = 0;
+  const subordinates = adjList[currentId];
+  for(let i = 0; i < subordinates.length; i++) {
+    max = Math.max(max, dfs(subordinates[i], adjList, informTime));
+  }
+  
+  return max + informTime[currentId];
+}
