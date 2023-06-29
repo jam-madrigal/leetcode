9b885adf40bb45c2985b3e8251bf9a6af18a7309
Alex <jam.madrigal@gmail.com>
2023-06-28 19:56:37 -0700

    Time: 60 ms (98.93%), Space: 46.1 MB (91.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,40 +4,44 @@
  * @return {boolean}
  */
 const canFinish = function(n, prerequisites) {
-  // We make so many loops that way it starts fresh again if it reaches the end of one bfs, in some cases it might build the whole thing in one go, but every vertex/starting position has to be checked since the graphs can be unconnected, this makes it less efficient and a brute force solution...
-  // First thing to do is make an adjacency list out of the list of prerequisites
-  // Create an array then fill it with empty arrays, required for javascript
-  const adjList = new Array(n).fill(0).map(() => []);
+  // Topological sort optimized solution
+  // Make an inDegree array which says how many connections a given vertex has
+  const inDegree = new Array(n).fill(0);
+  // Also build out an adjacency list to keep track, can be built from inDegree since it requires some of the same stops, just replace the 0's with empty arrays
+  const adjList = inDegree.map(() => []);
+  // Loop through the pre-requisites and build the inDegree and adjList, every time a connection to a vertex is found, increase the inDegree value by 1, for the adjList push the connection to its respective index which represents the vertex to which it connects
   for (let i = 0; i < prerequisites.length; i++) {
     const pair = prerequisites[i];
+    inDegree[pair[0]]++;
+
     adjList[pair[1]].push(pair[0]);
   }
-  // Then, using that adjacency list, run bfs on each possible starting point to see if there is ever a cycle, a cycle means the courses cannot be finished because some courses are pre-requisites for each other, making it impossible to do them all
-  // v = vertex
-  // Loop through the adjency list to keep track of every possible starting point
-  for (v = 0; v < n; v++) {
-    const queue = [];
-    const seen = {};
-    // Loop through the individual matrices in the adjacency list, keeping track of what nodes have been seen
-    // return false if you reach the starting node
-    // restart the checking from every possible starting point (topological search will make this more efficient)
-    for (let i = 0; i < adjList[v].length; i++) {
-      queue.push(adjList[v][i]);
+  // To be more efficient, we can make another data structure that holds all the values so far that have an inDegree of 0, so we know which ones to pop off next, instead of looping through it every time to find which has 0. The inDegrees of 0 don't have any connections left, so we know they can be completed at this point
+  const stack = [];
+  // Loop through the inDegree length, find all the current values of 0, push them to the stack. Then, as we reduce other values, we can check if they're now 0 and then push them to the stack too as we go on using a while loop
+  for (let i = 0; i < inDegree.length; i++) {
+    if (inDegree[i] === 0) {
+      stack.push(i);
     }
-  // Now we can start actually checking for cycles
-  while (queue.length) {
-    current = queue.shift();    
-    seen[current] = true;
-    // v is the starting point, so if ever the queue we generated hits v again... we know it's a cycle
-    if (current === v) return false;
-    // Loop through all the connections for the currently queued vertex to continue making the queue
+  }
+  // Loop through the stack, popping from it, then checking with the adjacency list which vertexes that connection is connected to. Reduce the inDegree of those vertexes by 1, check if they are equal to 0, and if they are, push them to the stack
+    // We know that if we get through every single connection and some vertexes still have connections, then there must be a cycle, so we can return false, otherwise return true. We can know this by checking if we touched every vertex based on the passed in n value, if the stack doesn't get pushed to as many times as there are n, then there must be a cycle and the courses must not be completeable 
+  let count = 0;
+  while (stack.length) {
+    const current = stack.pop();
+    count++;
+    // Grab the adjacent array to the current vertex with an inDegree of 0 and loop through it
     const adjacent = adjList[current];
     for (let i = 0; i < adjacent.length; i++) {
-      if (!seen[adjacent[i]]) {
-        queue.push(adjacent[i]);
+      // If the current vertex connects to anything, decrement the inDegree of those connections by 1
+      const next = adjacent[i];
+      inDegree[next]--;
+      // If the inDegree is now 0, push it to the stack to be processed
+      if (inDegree[next] === 0) {
+        stack.push(next);
       }
     }
   }
+  // Now that everything is sorted, we can return if the courses can all be completed or not. We return true or falase based on the count value.
+  return count === n;
 }
-  return true;
-};
