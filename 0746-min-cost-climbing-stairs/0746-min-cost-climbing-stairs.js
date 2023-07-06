eacb574e015003c2d55bc22bfd3022ae80064f5b
Alex <jam.madrigal@gmail.com>
2023-07-06 16:00:49 -0700

    Time: 56 ms (90.48%), Space: 41.9 MB (95.40%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -7,16 +7,16 @@ var minCostClimbingStairs = function(cost) {
     // Utilize the same dynamic programming memoizatoin idea to save what's already been calculated
     const dp = [];
     const n = cost.length;
+    // Further optimize by improving space by updating variables instead of storing every single minimum cost, since we never use them after calculating them until the very end
+    let dpOne = cost[0];
+    let dpTwo = cost[1];
     // Starting from the bottom, calculate up the graph, saving the minimum steps you already found then using those values to do further calculations if they're already known
-    for (let i = 0; i < n; i++) {
-        // No reason to calculate the first two indices minimum cost as there is only one possibility
-        if (i < 2) {
-            dp[i] = cost[i];
-        } else {
-            // Calculates the minimum cost and stores it
-            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
-        }
+    for (let i = 2; i < n; i++) {
+        const current = cost[i] + Math.min(dpOne, dpTwo);
+        // Only save the two most recent values, since we'll only need the last two in the end. If you think about the tree data structure, only these two previous values matter for calculating the next, as they are the two steps that can lead to it.
+        dpOne = dpTwo;
+        dpTwo = current;
     }
-    return Math.min(dp[n-1], dp[n-2]);
+    return Math.min(dpOne, dpTwo);
 };
 
