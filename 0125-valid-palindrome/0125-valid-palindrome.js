8d417629da441164e57e4c7cc787d6d20e0d9e9e
Alex <jam.madrigal@gmail.com>
2023-07-17 17:36:29 -0700

    Time: 60 ms (96.24%), Space: 44.1 MB (88.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,24 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  // Two pointers from center, can also move them from the ends, or compare against the reverse by making two new strings from either end, after replacing, and comparing, each has advantages depending on the situation...
-  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-  // Find the center of the string
-  center = s.length / 2;
-  // Start the pointers at the same center point rounded down, Check if the length is even, and if it is, move the left pointer back by 1 so they are equidistant from the respective ends if it is indeed a palindrome
-  let left = Math.floor(center);
-  let right = left;
-  if (s.length % 2 === 0) {
-    left--;
-  }
-  // While the left pointer is greater than or equal to 0 (because it can reach 0 sooner than the right pointer reaches the end of the string) or the right pointer is not equal to the end, loop through and compare if the values are equal
-  while (left >= 0 && right < s.length) {
-    if (s[left] !== s[right]) {
-      return false 
-    }
-    left--;
-    right++;
-  }
-  // return true if the while loop manages to reach the ends of the string
-  return true;
+    // TODO: Write your code here
+    // recreate the string, regex to remove all non-alphanumeric characters
+    s = s.replace(/[\W_]+/g,"").toLowerCase();
+    // set to lowercase
+    // instead of looping through two of the same string, two pointer and start from back and front
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+      if (s[left] !== s[right]) return false;
+      left++;
+      right--;
+    } 
+    // Increment each one one by one and if they are ever not equal return false, otherwise return true
+    // Stop the loop when they are at the same index
+    return true;
 };
